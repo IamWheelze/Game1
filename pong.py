c78ed2e18abb41e25fe8b44f986731a24980b6fd
IamWheelze <129524962+IamWheelze@users.noreply.github.com>
2025-09-01 18:46:46 +0200

    Create pong.py

---
+++ pong.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.screen()
+wn.title('ping by joshua')
+wn.bgcolor('blue')
+wn,setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
+
+# PLayer one
